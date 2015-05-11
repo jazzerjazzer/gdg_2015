b09d173175faefd814a4bc0d72bef2caec158659
alimgiray <alimgiray@gmail.com>
2015-05-11 18:07:37 +0300

    Android Launch

--- android/src/com/pikseloyun/gdg2015/android/AndroidLauncher.java
+++ android/src/com/pikseloyun/gdg2015/android/AndroidLauncher.java
@@ -10,7 +10,7 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainGame(), config);
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();        
+        initialize(new MainGame(), cfg);
 	}
 }
